be02915385e8a64e97bef3a2568ee4ad2973ace4
Lily Hopkins <lilopkins@outlook.com>
2021-02-27 22:55:46 +0000

    Added logger initialisation to examples

--- examples/create_lnk.rs
+++ examples/create_lnk.rs
@@ -2,6 +2,8 @@ use std::io::Result;
 use std::path::Path;
 
 fn main() -> Result<()> {
+    pretty_env_logger::init();
+    
     let shortcut = lnk::ShellLink::new_simple(Path::new(r"C:\Windows\System32\notepad.exe"))?;
     shortcut.save("np.lnk").expect("Failed to save shortcut!");
     Ok(())
--- examples/read_lnk.rs
+++ examples/read_lnk.rs
@@ -1,6 +1,8 @@
 use std::env;
 
 fn main() {
+    pretty_env_logger::init();
+    
     let args: Vec<String> = env::args().collect();
 
     if args.len() == 1 {
