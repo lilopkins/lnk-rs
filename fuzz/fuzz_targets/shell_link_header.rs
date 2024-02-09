bf8990c922a3edee40957a249e2e11cae56feaa3
Jan Starke <jan.starke@posteo.de>
2024-02-09 17:35:46 +0100

    fuzzing works

--- fuzz/fuzz_targets/shell_link_header.rs
+++ fuzz/fuzz_targets/shell_link_header.rs
@@ -1,7 +1,14 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
+use lnk::ShellLinkHeader;
+use std::io::Cursor;
+use binread::BinReaderExt;
 
 fuzz_target!(|data: &[u8]| {
-    let _shell_link_header = lnk::ShellLinkHeader::try_from(&data[0..0x4c]);
+    let mut cursor = Cursor::new(data);
+    match cursor.read_le::<ShellLinkHeader>() {
+        Err(_) => (),
+        Ok(header) => println!("fuzzer found as valid header of size {}", header.header_size())
+    }
 });
