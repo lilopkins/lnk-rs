7d064e3871581fbcb67d78d37e6be7238f5f571f
Lily Hopkins <lily@hpkns.uk>
2025-08-30 20:36:20 +0100

    0.6.2 (#32)
    * fix: disabled an assertion during docs build
    
    * chore: bumped version

--- src/linkinfo.rs
+++ src/linkinfo.rs
@@ -139,8 +139,8 @@ pub struct LinkInfo {
     /// CommonPathSuffixOffset (4 bytes): A 32-bit, unsigned integer that
     /// specifies the location of the CommonPathSuffix field. This value is
     /// an offset, in bytes, from the start of the LinkInfo structure.
-    #[br(assert(common_path_suffix_offset < link_info_size &&
-                common_path_suffix_offset >= link_info_header_size))]
+    #[cfg_attr(not(docsrs), br(assert(common_path_suffix_offset < link_info_size
+        && common_path_suffix_offset >= link_info_header_size)))]
     common_path_suffix_offset: u32,
 
     /// LocalBasePathOffsetUnicode (4 bytes): An optional, 32-bit, unsigned
